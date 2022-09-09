5dc32186e3a0c0a5c91163a1ceded14a54b3d1bb
Fer Araujo <fer_araujo@live.com.mx>
2022-09-09 03:13:39 -0500

    Final Commit (adding last minute styles)

--- src/Components/Error/Error.js
+++ src/Components/Error/Error.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { useNavigate } from "react-router";
 import { useTheme } from "../Helpers/ThemeContext";
+import "./Error.css";
 
 const Error = () => {
   const history = useNavigate();
@@ -8,28 +9,30 @@ const Error = () => {
   const isLogged = localStorage.getItem("isLogged");
   return (
     <div className={`App__error-${theme}`}>
-      <h1 className={`App__error-title-${theme}`}>
-        Error 404, The site does not exist!
-      </h1>
-      {!isLogged ? (
-        <button
-          className={`App__error-button-${theme}`}
-          onClick={() => {
-            history("/login");
-          }}
-        >
-          Back to Login
-        </button>
-      ) : (
-        <button
-          className={`App__error-button-${theme}`}
-          onClick={() => {
-            history("/home");
-          }}
-        >
-          Back to Home
-        </button>
-      )}
+      <div className="App__error-container">
+        <h1 className={`App__error-title`}>
+          Error 404, The site does not exist!
+        </h1>
+        {!isLogged ? (
+          <button
+            className={`App__error-button`}
+            onClick={() => {
+              history("/login");
+            }}
+          >
+            Back to Login
+          </button>
+        ) : (
+          <button
+            className={`App__error-button`}
+            onClick={() => {
+              history("/home");
+            }}
+          >
+            Back to Home
+          </button>
+        )}
+      </div>
     </div>
   );
 };
